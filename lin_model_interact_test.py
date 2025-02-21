eb980ebc7056db2b1937b585b67b7bc399567dc6
dirk <dirknbr@gmail.com>
2025-02-21 10:04:39 +0000

    Update lin_model_interact_test.py

--- lin_model_interact_test.py
+++ lin_model_interact_test.py
@@ -6,6 +6,7 @@ class TestLinModelInteract(unittest.TestCase):
 
   def test_end_to_end(self):
     pairs0 = [(1, 2), (3, 4), (6, 7)]
+    np.random.seed(33)
     X, y = sim(n=1000, p=10, pairs=pairs0)
     res, pairs1 = find_model(X, y, 5)
     pairs_matched = count_matching_pairs(pairs0, pairs1)
